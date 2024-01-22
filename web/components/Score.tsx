2c7ae1c71b7fbbcb29828bb97936ea8c0564e21f
Cesar <cesarbrazon10@gmail.com>
2024-01-22 13:09:50 +0100

    chore: add button to open analysis

--- web/components/Score.tsx
+++ web/components/Score.tsx
@@ -1,4 +1,5 @@
 import HighScoreIcon from "@/public/high-score-icon.svg";
+import CopyToClipboardIcon from "@/public/copy-to-clipboard.svg"
 import Image from "next/image";
 
 export default function Score({ rank }: { rank: number }) {
@@ -11,6 +12,9 @@ export default function Score({ rank }: { rank: number }) {
           <Image alt="score" priority src={Icon} />
         </div>
         <div>{(rank * 100).toFixed(2)}</div>
+        <div>
+          <Image className="hover:cursor-pointer" alt="copy-clipboard" src={CopyToClipboardIcon} />
+        </div>
       </div>
     );
   }
