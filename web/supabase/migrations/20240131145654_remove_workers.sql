a2e8cf88724464fc9919658d63ff4c9abb72e4ab
dOrgJelli <jelli@dorg.tech>
2024-01-31 19:02:49 +0100

    chore: migrate user_id into runs table

--- web/supabase/migrations/20240131145654_remove_workers.sql
+++ web/supabase/migrations/20240131145654_remove_workers.sql
@@ -1,3 +1,7 @@
+CREATE TEMP TABLE tmp_worker_user_mapping AS
+SELECT id AS worker_id, user_id
+FROM "public"."workers";
+
 drop policy "Users can only insert their own runs" on "public"."runs";
 drop policy "Everyone can see all workers" on "public"."workers";
 drop policy "Users can only insert workers of their own" on "public"."workers";
@@ -6,12 +10,22 @@ alter table "public"."runs" drop constraint "runs_worker_id_fkey";
 alter table "public"."workers" drop constraint "workers_user_id_fkey";
 
 alter table "public"."workers" drop constraint "workers_pkey";
-
 drop index if exists "public"."workers_pkey";
 drop table "public"."workers";
 
+alter table "public"."runs" add column "user_id" uuid;
+
+UPDATE "public"."runs"
+SET user_id = tmp.user_id
+FROM tmp_worker_user_mapping tmp
+WHERE "public"."runs".worker_id = tmp.worker_id;
+
+alter table "public"."runs" alter column "user_id" set not null;
+
+DROP TABLE tmp_worker_user_mapping;
+
 alter table "public"."runs" drop column "worker_id";
-alter table "public"."runs" add column "user_id" uuid not null;
+
 alter table "public"."runs" add constraint "runs_user_id_fkey" FOREIGN KEY (user_id) REFERENCES users(id) not valid;
 alter table "public"."runs" validate constraint "runs_user_id_fkey";
 alter table "public"."runs" alter column "user_id" set default auth.uid();
