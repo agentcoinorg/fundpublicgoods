9e7629ef14954e58c81f8ed0ccf5841448ab82df
Nestor Amesty <nestor09amesty@gmail.com>
2024-02-05 18:11:40 +0100

    Camel case alias for smart ranking

--- workers/fund_public_goods/lib/strategy/models/weighted_project.py
+++ workers/fund_public_goods/lib/strategy/models/weighted_project.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict, Field
 from fund_public_goods.lib.strategy.models.project import Project
 from fund_public_goods.lib.strategy.models.project_scores import ProjectScores
 
@@ -7,7 +7,11 @@ class WeightedProject(BaseModel):
     report: str
     scores: ProjectScores
     weight: float
-    smart_ranking: float
+    smart_ranking: float = Field(..., alias="smartRanking")
 
     def __getitem__(self, item):
         return getattr(self, item)
+    
+    model_config = ConfigDict(
+        populate_by_name=True,
+    )
