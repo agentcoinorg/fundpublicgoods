e6c7f9a2d71ef08afbed00e1c8894f3b9cbc6c56
dOrgJelli <jelli@dorg.tech>
2024-02-07 05:04:56 +0100

    revert

--- workers/fund_public_goods/api/runs.py
+++ workers/fund_public_goods/api/runs.py
@@ -5,9 +5,7 @@ from fastapi import APIRouter, HTTPException, Header, BackgroundTasks
 from pydantic import BaseModel
 from typing import Optional
 import os
-import asyncio
 import httpx
-
 
 api_url = os.getenv("WORKERS_URL")
 router = APIRouter()
@@ -15,36 +13,28 @@ router = APIRouter()
 class Params(BaseModel):
     prompt: str
 
+
 class Response(BaseModel):
     run_id: str
 
 async def run(run_id: str, authorization: str):
-    client = httpx.AsyncClient()
-
-    async def task():
-        try:
-            await client.post(
-                f"{api_url}/api/runs/run",
-                json={"run_id": run_id},
-                headers={"Authorization": authorization}
-            )
-        finally:
-            await client.aclose()
-
-    asyncio.create_task(task())
+    async with httpx.AsyncClient() as client:
+        await client.post(
+            f"{api_url}/api/runs/run",
+            json={"run_id": run_id},
+            headers={"Authorization": authorization}
+        )
 
 @router.post("/api/runs")
-async def runs(params: Params, authorization: Optional[str] = Header(None)) -> Response:
+async def runs(background_tasks: BackgroundTasks, params: Params, authorization: Optional[str] = Header(None)) -> Response:
     if authorization:
         supabase_auth_token = authorization.split(" ")[1]
     else:
         raise HTTPException(status_code=401, detail="Authorization header missing")
     
     prompt = params.prompt if params.prompt else ""
-
     if prompt == "":
         raise HTTPException(status_code=400, detail="Prompt cannot be empty.")
-
     db = client.create(options=ClientOptions())
     db.postgrest.auth(supabase_auth_token)
     
@@ -53,6 +43,6 @@ async def runs(params: Params, authorization: Optional[str] = Header(None)) -> R
     ), db)
     initialize_logs(run_id)
 
-    await run(run_id, authorization)
+    background_tasks.add_task(run, run_id, authorization)
 
     return Response(run_id=run_id)
