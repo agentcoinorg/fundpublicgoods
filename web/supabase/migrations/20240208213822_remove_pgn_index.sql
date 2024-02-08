d6ceb7f91218a93a0bf4d508b1d16c272aa50c11
Cesar <cesarbrazon10@gmail.com>
2024-02-08 23:01:57 +0100

    chore: dont add pgn in gitcoin index job

--- web/supabase/migrations/20240208213822_remove_pgn_index.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM "indexing"."gitcoin_indexing_jobs" WHERE "network_id" = 424;
-DELETE FROM "indexing"."gitcoin_applications" WHERE "network" = 424;
-DELETE FROM "public"."applications" WHERE network = 424;
