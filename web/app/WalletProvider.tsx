8ffe3a5884034392612de47606704e91b97913be
Cesar <cesarbrazon10@gmail.com>
2024-02-09 01:55:03 +0100

    chore: handle different chain in transaction send & remove transaction view

--- web/app/WalletProvider.tsx
+++ web/app/WalletProvider.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Web3OnboardProvider, init } from "@web3-onboard/react";
 import injectedModule from "@web3-onboard/injected-wallets";
@@ -7,8 +7,56 @@ const chains = [
   {
     id: "0x1",
     token: "ETH",
-    label: "Ethereum Mainnet",
+    label: "Mainnet",
     rpcUrl: `https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_PROJECT_ID}`,
+  },
+  {
+    id: "0xa86a",
+    token: "AVAX",
+    label: "Avalanche",
+    rpcUrl: `https://avalanche-mainnet.infura.io/v3/1a8e6a8ab1df44ccb77d3e954082c5d4`,
+  },
+  {
+    id: "0xa",
+    token: "ETH",
+    label: "Optimism",
+    rpcUrl: `https://optimism-mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_PROJECT_ID}`,
+  },
+  {
+    id: "0xa4b1",
+    token: "ETH",
+    label: "ArbitrumOne",
+    rpcUrl: `https://arbitrum-mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_PROJECT_ID}`,
+  },
+  {
+    id: "0x89",
+    token: "MATIC",
+    label: "Polygon",
+    rpcUrl: `https://polygon-mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_PROJECT_ID}`,
+  },
+  {
+    id: "0x2105",
+    token: "ETH",
+    label: "Base",
+    rpcUrl: `https://base.llamarpc.com`,
+  },
+  {
+    id: "0x144",
+    token: "ETH",
+    label: "zkSync",
+    rpcUrl: `0x144`,
+  },
+  {
+    id: "0xfa",
+    token: "FTM",
+    label: "FantomOpera",
+    rpcUrl: `https://1rpc.io/ftm`,
+  },
+  {
+    id: "0xe298",
+    token: "ETH",
+    label: "Sepolia",
+    rpcUrl: `https://sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_PROJECT_ID}`,
   },
 ];
 const wallets = [injectedModule()];
@@ -18,7 +66,7 @@ const web3Onboard = init({
   appMetadata: {
     name: "Fund Public Goods",
     icon: "<svg>App Icon</svg>",
-    description: "cool description"
+    description: "cool description",
   },
 });
 
