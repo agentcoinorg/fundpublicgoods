7fea36a66217b6f84ca2c5e8952514601ad51753
Cesar <cesarbrazon10@gmail.com>
2024-02-12 20:06:37 +0100

    chore: add chain logos + disconnect function

--- web/app/Header.tsx
+++ web/app/Header.tsx
@@ -1,20 +1,35 @@
 "use client";
 
-import Dropdown from "@/components/Dropdown";
 import { Logo } from "@/components/Logo";
 import truncateEthAddress from "@/utils/ethereum/truncateAddress";
 import { CaretDown, Link } from "@phosphor-icons/react";
 import { useConnectWallet } from "@web3-onboard/react";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function Header() {
-  const [{ wallet }] = useConnectWallet();
+  const [{ wallet }, _, disconnect] = useConnectWallet();
   const [showDropdownMenu, setShowDropdownMenu] = useState<boolean>(false);
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
 
-  const handleClick = () => {
-    setShowDropdownMenu(!showDropdownMenu);
-  };
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      const current = dropdownRef.current;
+      if (current && !current.contains(event.target as Node)) {
+        setShowDropdownMenu(false);
+      }
+    }
+
+    // Bind the event listener
+    if (showDropdownMenu) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    // Unbind the event listener on clean up
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [showDropdownMenu]);
 
   return (
     <div className='flex w-full justify-between text-sm px-6 py-4 bg-indigo-300/80 border-b-2 border-indigo-400/20'>
@@ -22,9 +37,9 @@ function Header() {
         <Logo size={160} />
       </a>
       {wallet && (
-        <div className='relative'>
+        <div className='relative' ref={dropdownRef}>
           <div
-            onClick={handleClick}
+            onClick={() => setShowDropdownMenu((current) => !current)}
             className='text-indigo-600 hover:text-indigo-800 font-bold text-sm cursor-pointer flex space-x-1 items-center p-1 rounded-md hover:bg-indigo-500/20'>
             <div>
               {wallet.accounts[0]?.ens?.name ? (
@@ -46,16 +61,22 @@ function Header() {
             </div>
             <CaretDown weight='bold' />
           </div>
-          {showDropdownMenu && <div className='absolute top-[calc(100%+8px)] right-0 z-10'>
+          {showDropdownMenu ? <div className='absolute top-[calc(100%+8px)] right-0 z-10' >
             <div className='bg-indigo-50 rounded-2xl border-2 border-indigo-300 space-y-2 p-3 shadow-xl shadow-primary-shadow/20'>
-              <div className='p-2 flex items-center space-x-2 hover:bg-white cursor-pointer w-full rounded-xl'>
+              <div
+                className="p-2 flex items-center space-x-2 hover:bg-white cursor-pointer w-full rounded-xl"
+                onClick={() => {
+                  disconnect(wallet);
+                  setShowDropdownMenu(!showDropdownMenu)
+                }}
+              >
                 <Link size={16} weight='bold' className='min-w-[16px]' />
                 <div className='whitespace-nowrap'>
                   Disconnect Wallet
                 </div>
               </div>
             </div>
-          </div>}
+          </div> : null}
         </div>
       )}
     </div>
