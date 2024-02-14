c6bd127dfb2b280968c5a142803ca1a8a6c6eee5
Nestor Amesty <nestor09amesty@gmail.com>
2024-02-14 04:44:59 +0100

    Categories moved to constant

--- workers/fund_public_goods/lib/strategy/utils/categorize_project.py
+++ workers/fund_public_goods/lib/strategy/utils/categorize_project.py
@@ -1,3 +1,4 @@
+from fund_public_goods.lib.strategy.utils.constants import PROJECT_CATEGORIES
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
@@ -6,106 +7,7 @@ from langchain_core.output_parsers import StrOutputParser
 categorize_prompt_template = """
 Your goal is to categorize a project according to it's description. These are the existing categories:
 
-- Decentralized Finance (DeFi)
-- Artificial Intelligence (AI) Integration
-- Blockchain Technology and Ecosystems
-- Tokenomics and Cryptocurrency
-- NFT (Non-Fungible Tokens) Marketplace and Integration
-- Environmental Sustainability and Regenerative Practices
-- Education and Knowledge Dissemination
-- Health and Wellness
-- Social Impact and Philanthropy
-- Community Building and Governance
-- Privacy and Security in Blockchain
-- Gaming and Gamification
-- Accessibility and Inclusivity
-- Art, Music, and Cultural Preservation
-- Legal and Regulatory Compliance
-- Infrastructure and Tooling for Blockchain
-- Research and Development in Blockchain
-- Space Exploration and Technology
-- Agriculture and Food Security
-- Climate Action and Carbon Market
-- Web3 Adoption and Education
-- Public Goods and Civic Engagement
-- Financial Tools and Services
-- User Experience and Interface Design
-- Conservation and Wildlife Protection
-- Entrepreneurship and Business Development
-- Science and Technology
-- Energy and Renewable Resources
-- Event Organization and Participation
-- Translation and Multilingual Support
-- Content Creation and Media Production
-- Mental Health and Empowerment
-- DAOs (Decentralized Autonomous Organizations)
-- E-commerce and Retail Integration
-- Software Development and Open Source
-- Real Estate and Asset Tokenization
-- Supply Chain Management and Transparency
-- Digital Identity and Verification
-- Networking and Professional Development
-- Volunteerism and Community Support
-- Career Development and Job Market
-- Marketing and Branding Strategy
-- Hardware Technology and Integration
-- Data Analytics and Visualization
-- Healthcare Integration and Innovation
-- Grant Funding Mechanisms and Support
-- Cybersecurity and Ethical Hacking
-- Outreach and Advocacy
-- Economic Empowerment and Microfinance
-- Zero-Knowledge Proofs and Cryptography
-- Intellectual Property and Copyright
-- Risk Management and Insurance
-- Quantum Computing and Advanced Technologies
-- Human Rights and Ethical Practices
-- Travel and Tourism
-- Urban Development and Smart Cities
-- Payment Systems and Digital Wallets
-- Social Networking and Digital Communities
-- Automation and Robotics
-- Fashion and Textiles
-- Conservation Through Gaming and VR
-- Remote Work and Digital Nomadism
-- Food and Beverage Industry
-- Transportation and Mobility
-- Waste Management and Recycling
-- Disaster Relief and Crisis Management
-- Youth Engagement and Education
-- Mental Health and Counseling
-- Women Empowerment and Gender Equality
-- Indigenous Rights and Cultural Heritage
-- Digital Rights and Internet Freedom
-- Digital Sovereignty and Data Autonomy
-- Social Justice and Equal Opportunity
-- Livelihood Enhancement and Skills Training
-- Mental Health Advocacy and Support Services
-- Sustainable Agriculture and Permaculture
-- Cryptocurrency Adoption and Integration
-- Interoperability and Cross-Chain Solutions
-- Climate Advocacy and Policy Influence
-- Circular Economy and Sustainable Practices
-- Veterinary Services and Animal Health
-- Accessibility for People with Disabilities
-- Humanitarian Aid and Refugee Support
-- Local Community Initiatives and Empowerment
-- Child Welfare and Development
-- Public Health Initiatives and Disease Prevention
-- Substance Abuse Prevention and Rehabilitation
-- Financial Literacy and Consumer Education
-- Digital Archiving and Historic Preservation
-- Alternative Energy and Green Tech
-- Public Transportation and Infrastructure
-- Mental Health Research and Neurodiversity
-- Military Veteran Support and Reintegration
-- Cultural Festivals and Creative Events
-- Digital Literacy and Computer Education
-- Affordable Housing and Community Development
-- Academic Research and Scholarly Publishing
-- Social Entrepreneurship and Cooperative Business
-- Mental Health Awareness and Education
-- Digital Inclusion and Tech Accessibility
+{categories}
 
 Consider that all projects are within the blockchain ecosystem and seeking public goods funding.
 A project can have multiple categories. Be strict with assigning categories.
@@ -130,7 +32,8 @@ def categorize_project(project_descriptions: list[str]) -> list[list[str]]:
     categorize_chain = categorize_prompt | llm | StrOutputParser()
 
     category_strings = categorize_chain.batch([{
-        "project_description": project_description
+        "project_description": project_description,
+        "categories": "\n".join(f"- {category}" for category in PROJECT_CATEGORIES)
     } for project_description in project_descriptions])
     
     categories: list[list[str]] = []
--- workers/fund_public_goods/lib/strategy/utils/categorize_prompt.py
+++ workers/fund_public_goods/lib/strategy/utils/categorize_prompt.py
@@ -1,3 +1,4 @@
+from fund_public_goods.lib.strategy.utils.constants import PROJECT_CATEGORIES
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
@@ -6,106 +7,7 @@ from langchain_core.output_parsers import StrOutputParser
 categorize_prompt_template = """
 Your goal is to categorize a user's prompt. These are the existing categories:
 
-- Decentralized Finance (DeFi)
-- Artificial Intelligence (AI) Integration
-- Blockchain Technology and Ecosystems
-- Tokenomics and Cryptocurrency
-- NFT (Non-Fungible Tokens) Marketplace and Integration
-- Environmental Sustainability and Regenerative Practices
-- Education and Knowledge Dissemination
-- Health and Wellness
-- Social Impact and Philanthropy
-- Community Building and Governance
-- Privacy and Security in Blockchain
-- Gaming and Gamification
-- Accessibility and Inclusivity
-- Art, Music, and Cultural Preservation
-- Legal and Regulatory Compliance
-- Infrastructure and Tooling for Blockchain
-- Research and Development in Blockchain
-- Space Exploration and Technology
-- Agriculture and Food Security
-- Climate Action and Carbon Market
-- Web3 Adoption and Education
-- Public Goods and Civic Engagement
-- Financial Tools and Services
-- User Experience and Interface Design
-- Conservation and Wildlife Protection
-- Entrepreneurship and Business Development
-- Science and Technology
-- Energy and Renewable Resources
-- Event Organization and Participation
-- Translation and Multilingual Support
-- Content Creation and Media Production
-- Mental Health and Empowerment
-- DAOs (Decentralized Autonomous Organizations)
-- E-commerce and Retail Integration
-- Software Development and Open Source
-- Real Estate and Asset Tokenization
-- Supply Chain Management and Transparency
-- Digital Identity and Verification
-- Networking and Professional Development
-- Volunteerism and Community Support
-- Career Development and Job Market
-- Marketing and Branding Strategy
-- Hardware Technology and Integration
-- Data Analytics and Visualization
-- Healthcare Integration and Innovation
-- Grant Funding Mechanisms and Support
-- Cybersecurity and Ethical Hacking
-- Outreach and Advocacy
-- Economic Empowerment and Microfinance
-- Zero-Knowledge Proofs and Cryptography
-- Intellectual Property and Copyright
-- Risk Management and Insurance
-- Quantum Computing and Advanced Technologies
-- Human Rights and Ethical Practices
-- Travel and Tourism
-- Urban Development and Smart Cities
-- Payment Systems and Digital Wallets
-- Social Networking and Digital Communities
-- Automation and Robotics
-- Fashion and Textiles
-- Conservation Through Gaming and VR
-- Remote Work and Digital Nomadism
-- Food and Beverage Industry
-- Transportation and Mobility
-- Waste Management and Recycling
-- Disaster Relief and Crisis Management
-- Youth Engagement and Education
-- Mental Health and Counseling
-- Women Empowerment and Gender Equality
-- Indigenous Rights and Cultural Heritage
-- Digital Rights and Internet Freedom
-- Digital Sovereignty and Data Autonomy
-- Social Justice and Equal Opportunity
-- Livelihood Enhancement and Skills Training
-- Mental Health Advocacy and Support Services
-- Sustainable Agriculture and Permaculture
-- Cryptocurrency Adoption and Integration
-- Interoperability and Cross-Chain Solutions
-- Climate Advocacy and Policy Influence
-- Circular Economy and Sustainable Practices
-- Veterinary Services and Animal Health
-- Accessibility for People with Disabilities
-- Humanitarian Aid and Refugee Support
-- Local Community Initiatives and Empowerment
-- Child Welfare and Development
-- Public Health Initiatives and Disease Prevention
-- Substance Abuse Prevention and Rehabilitation
-- Financial Literacy and Consumer Education
-- Digital Archiving and Historic Preservation
-- Alternative Energy and Green Tech
-- Public Transportation and Infrastructure
-- Mental Health Research and Neurodiversity
-- Military Veteran Support and Reintegration
-- Cultural Festivals and Creative Events
-- Digital Literacy and Computer Education
-- Affordable Housing and Community Development
-- Academic Research and Scholarly Publishing
-- Social Entrepreneurship and Cooperative Business
-- Mental Health Awareness and Education
-- Digital Inclusion and Tech Accessibility
+{categories}
 
 A user's prompt can match to more than one category. Be strict with assigning categories.
 Return a max of {n} categories, you can reutrn less if the project really only matches less than {n} categories.
@@ -127,7 +29,8 @@ def categorize_prompt(prompt: str, n: int) -> list[str]:
 
     categories = [c.strip() for c in categorize_chain.invoke({
         "prompt": prompt,
-        'n': n
+        'n': n,
+        "categories": "\n".join(f"- {category}" for category in PROJECT_CATEGORIES)
     }).split(',')]
             
     return categories
