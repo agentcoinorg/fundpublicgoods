cc3d6b4d03fc0c3a4e7a4d26d432297c3aed56da
Cesar <cesarbrazon10@gmail.com>
2024-02-18 15:09:26 +0100

    chore: set up with sqs builds and works locally

--- workers/fund_public_goods/events.py
+++ workers/fund_public_goods/events.py
@@ -1,13 +1,19 @@
+from dataclasses import dataclass
+from typing import Any
 from aws_lambda_typing.events import SQSEvent
 from fastapi_events.typing import Event as LocalEvent
 from fastapi_events.handlers.local import local_handler
 from fastapi_events.handlers.aws import SQSForwardHandler
 from fastapi_events.middleware import EventHandlerASGIMiddleware
-from fund_public_goods.lib.strategy import create
+from fund_public_goods.lib.strategy.create import create
+import os
+import json
+from fastapi import FastAPI
 
+@dataclass(kw_only=True)
 class EventData:
     name: str
-    payload: Dict[str, Any]
+    payload: dict[str, Any]
 
 def handler(event: EventData):
     if event.name == "create-strategy":
@@ -15,10 +21,10 @@ def handler(event: EventData):
         authorization = event.payload["authorization"]
         create(run_id, authorization)
     else:
-        throw Error("Unknown event name!")
+        raise Exception("Unknown event name!")
 
 @local_handler.register(event_name="*")
-def local_handler(local_event: LocalEvent):
+def local_handle(local_event: LocalEvent):
     event_name, payload = local_event
     event = EventData(name=event_name, payload=payload)
     handler(event)
@@ -28,32 +34,30 @@ def sqs_handler(sqs_event: SQSEvent):
 
     for record in sqs_event['Records']:
         message_body: str = record['body']
-        message: Dict[str, Any] = json.loads(message_body)
+        message: dict[str, Any] = json.loads(message_body)
         events.append(EventData(
-            name: message["name"],
-            payload: message["payload"]
+            message["name"],
+            message["payload"]
         ))
 
     for event in events:
         handler(event)
 
-def add_event_middleware(app):
+def add_event_middleware(app: FastAPI):
     event_handlers = []
+    env = os.getenv("RUNTIME")
 
-    if env.RUNTIME === "local"
-        event_handlers = [local_handler]
-    else if env.RUNTIME === "cloud":
+    if env == "cloud":
         event_handlers = [
             SQSForwardHandler(
                 queue_url="https://sqs.us-east-1.amazonaws.com/815880329304/fpg",
                 region_name="us-east-1"
             )
         ]
-
-    if len(event_handlers) == 0:
-        raise Exception(f"event_handlers should never have length 0")
+    else:
+        event_handlers = [local_handler]
 
     app.add_middleware(
         EventHandlerASGIMiddleware,
-        handlers=[event_handlers]
+        handlers=event_handlers
     )
